f0cb8d784e6024d88981d4a00bf80d589bf63575
akshay.wildfire / tw <akshay.wildfire@pm.me>
2022-06-10 00:18:33 +0530

    ember data connect

--- app/routes/types.js
+++ app/routes/types.js
@@ -1,3 +1,14 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class TypesRoute extends Route {}
+export default class TypesRoute extends Route {
+  @service store;
+
+  async model() {
+    //findRecord for type+slug pair and findAll all objects in a type
+    return await this.store.findRecord('types_json', 'types_json');
+
+    //this.store.findAll('film');
+    //this.store.query('film', {page: {limit: 2, offset: 0}});
+  }
+}
