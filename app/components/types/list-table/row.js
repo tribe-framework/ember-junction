dd8b4acf5ee1840ee82d470444aa7a846837fb2e
akshay.wildfire / tw <akshay.wildfire@pm.me>
2023-01-02 03:03:50 +0530

    type table data

--- app/components/types/list-table/row.js
+++ app/components/types/list-table/row.js
@@ -1,8 +1,9 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-//import { tracked } from '@glimmer/tracking';
+import { tracked } from '@glimmer/tracking';
 
 export default class TypesListTableRowComponent extends Component {
+
 	@action
 	showOptions() {
 		document.querySelector('#row-options-'+this.args.object.id).classList.remove('d-none');
@@ -12,4 +13,5 @@ export default class TypesListTableRowComponent extends Component {
 	hideOptions() {
 		document.querySelector('#row-options-'+this.args.object.id).classList.add('d-none');
 	}
+	
 }
--- app/controllers/types.js
+++ app/controllers/types.js
@@ -7,23 +7,11 @@ export default class TypesController extends Controller {
   @service store;
 
   @tracked currentType = null;
-  @tracked currentObject = null;
   @tracked objectsInType = null;
 
   @action
   loadTypeObjects(type) {
     this.currentType = type;
     this.objectsInType = this.store.findAll(type.slug, {show_public_objects_only: false});
-  }
-
-  @action
-  editObject(obj) {
-    if (obj !== "")
-      this.currentObject = obj;
-    else
-      this.currentObject = null;
-
-    const editObjectModal = new bootstrap.Modal('#editObjectModal', {});
-    editObjectModal.show();
   }
 }
