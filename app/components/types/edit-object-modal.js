88f5f0a90935f02135217699fe53bcc44651f360
akshay.wildfire / tw <akshay.wildfire@pm.me>
2023-01-03 15:19:36 +0530

    create, save and delete record using ember data

--- app/components/types/edit-object-modal.js
+++ app/components/types/edit-object-modal.js
@@ -8,12 +8,42 @@ export default class TypesEditObjectModalComponent extends Component {
   @tracked objectModules = this.args.object ? this.args.object.modules : {};
 
   @action
-  saveObject() {
+  pushObject() {
     let vvv = this.objectModules;
 
-    let obj = this.store.createRecord(this.args.type.slug, {
-      modules: vvv,
-    });
-    obj.save();
+    if (this.args.object !== null && this.args.object !== undefined && this.args.object.id !== null) {
+      this.store.findRecord(this.args.object.modules.type, this.args.object.modules.id).then((obj)=>{
+        obj.modules = vvv;
+        obj.save();
+      });
+    }
+
+    else {
+      let obj = this.store.createRecord(this.args.type.slug, {
+        modules: vvv,
+      });
+      obj.save();
+      //this.store.unloadAll(this.args.type.slug);
+    }
+  }
+
+  @action
+  deleteObject() {
+    if (this.args.object !== null && this.args.object !== undefined && this.args.object.id !== null) {
+      let obj = this.store.peekRecord(this.args.object.modules.type, this.args.object.modules.id);
+      obj.destroyRecord();
+    }
+  }
+
+  @action
+  areYouSure() {
+    document.querySelector('#deleteObjectConfirmation-'+this.args.object.id).classList.add('d-flex');
+    document.querySelector('#deleteObjectConfirmation-'+this.args.object.id).classList.remove('d-none');
+  }
+
+  @action
+  notSoSure() {
+    document.querySelector('#deleteObjectConfirmation-'+this.args.object.id).classList.add('d-none');
+    document.querySelector('#deleteObjectConfirmation-'+this.args.object.id).classList.remove('d-flex');
   }
 }
