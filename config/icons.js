e0d432633eff375855f0a9fb59b36d18f46079de
cityowl / jigmet <cityowl@hey.com>
2023-01-19 17:54:18 +0530

    fontawesome

---
+++ config/icons.js
@@ -0,0 +1,30 @@
+module.exports = function () {
+  return {
+    'pro-regular-svg-icons': ['ellipsis-vertical'],
+    'pro-light-svg-icons': [
+      'gauge-high',
+      'list-tree',
+      'upload',
+      'magnifying-glass',
+      'external-link-alt',
+      'gear',
+      'wand-magic-sparkles',
+      'timer',
+      'cloud-arrow-up',
+      'trash-alt',
+      'megaphone',
+      'magnifying-glass',
+      'plus',
+    ],
+    'pro-solid-svg-icons': [
+      'heart',
+      'plus',
+      'times',
+      'trash-alt',
+      'caret-down',
+      'chevrons-left',
+      'chevrons-right',
+    ],
+    'free-brands-svg-icons': [],
+  };
+};
