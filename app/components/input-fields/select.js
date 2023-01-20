120376e22ffc584b902708bfa66089d7d967eb05
cityowl / jigmet <cityowl@hey.com>
2023-01-20 14:16:00 +0530

    select when module name is same as type

--- app/components/input-fields/select.js
+++ app/components/input-fields/select.js
@@ -1,7 +1,13 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
 
 export default class InputFieldsSelectComponent extends Component {
+  @service store;
+
+  @tracked options = null;
+
   @action
   updateValue(e) {
     this.args.object[this.args.module.input_slug] = e.target.value;
@@ -11,5 +17,15 @@ export default class InputFieldsSelectComponent extends Component {
     return this.args.object[this.args.module.input_slug] !== undefined
       ? this.args.object[this.args.module.input_slug]
       : false;
+  }
+
+  @action
+  isModuleAlsoAType() {
+    //if this module is also a type append input_options from that type content
+    if (this.args.webapp.modules[this.args.module.input_slug] !== undefined) {
+      this.options = this.store.query(this.args.module.input_slug, {
+        page: { limit: -1 },
+      });
+    }
   }
 }
