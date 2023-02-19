18d9e01ef3a4a6cf4901579037f3104e203501cf
cityowl / jigmet <cityowl@hey.com>
2023-02-19 16:19:08 +0530

    delete unused models

--- app/models/section.js
+++
@@ -1,5 +0,0 @@
-import Model, { attr } from '@ember-data/model';
-
-export default class SectionModel extends Model {
-  @attr('tribe-modules') modules;
-}
--- app/models/user.js
+++
@@ -1,5 +0,0 @@
-import Model, { attr } from '@ember-data/model';
-
-export default class UserModel extends Model {
-  @attr('tribe-modules') modules;
-}
