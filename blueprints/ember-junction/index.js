81c3e1ca53935bd3d47b305e073b37c452f6e479
akshay.wildfire / tw <akshay.wildfire@pm.me>
2023-02-19 22:07:54 +0530

    normalizeEntityName

--- blueprints/ember-junction/index.js
+++ blueprints/ember-junction/index.js
@@ -1,15 +1,8 @@
 'use strict';
 
 module.exports = {
-  description: '',
-
-  // locals(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
+  normalizeEntityName() {},
+  
   // afterInstall(options) {
   //   // Perform extra work here.
   // }
