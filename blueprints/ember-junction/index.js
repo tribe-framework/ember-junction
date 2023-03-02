0e70cf461ce09730d0bf93e7eb661372d79c3d26
cityowl / jigmet <cityowl@hey.com>
2023-03-03 01:19:36 +0530

    editorjs plugins

--- blueprints/ember-junction/index.js
+++ blueprints/ember-junction/index.js
@@ -6,6 +6,8 @@ module.exports = {
   afterInstall(options) {
     return this.addPackagesToProject([
       { name: '@editorjs/editorjs' },
+      { name: '@editorjs/image' },
+      { name: '@editorjs/header' },
     ]);
   }
 };
