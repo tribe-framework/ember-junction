56504cf67432dcb3fcc92eb56c578cccf8da3a4f
cityowl / jigmet <cityowl@hey.com>
2023-05-04 13:49:33 +0530

    select heysack issue

--- blueprints/ember-junction/files/app/components/input-fields/select.js
+++ blueprints/ember-junction/files/app/components/input-fields/select.js
@@ -148,9 +148,11 @@ export default class InputFieldsSelectComponent extends Component {
     this.selectedMultiOptions = this.selectedMultiOptions;
 
     function inArray(needle, haystack) {
-      var length = haystack.length;
-      for (var i = 0; i < length; i++) {
-        if (haystack[i] == needle) return true;
+      if (haystack !== null) {
+        var length = haystack.length;
+        for (var i = 0; i < length; i++) {
+          if (haystack[i] == needle) return true;
+        }
       }
       return false;
     }
