cfb0a54cee076f784c35e9749efb3ab9f2da8343
cityowl / moti <cityowl@hey.com>
2023-09-21 23:24:50 +0530

    plausible integration

--- blueprints/ember-junction/files/app/controllers/index.js
+++ blueprints/ember-junction/files/app/controllers/index.js
@@ -1,6 +1,15 @@
 import Controller from '@ember/controller';
 import { service } from '@ember/service';
+import ENV from '<%= dasherizedPackageName %>/config/environment';
 
 export default class IndexController extends Controller {
   @service types;
+
+  get plausibleAuth() {
+    return ENV.PLAUSIBLE_AUTH;
+  }
+
+  get plausibleURL() {
+    return ENV.PLAUSIBLE_URL;
+  }
 }
--- blueprints/ember-junction/files/config/dotenv.js
+++ blueprints/ember-junction/files/config/dotenv.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 module.exports = function (/* env */) {
   return {
-    clientAllowedKeys: ['JUNCTION_PASSWORD'],
+    clientAllowedKeys: ['JUNCTION_PASSWORD', 'PLAUSIBLE_AUTH', 'PLAUSIBLE_URL'],
     fastbootAllowedKeys: [],
     failOnMissingKey: false,
     path: path.join(path.dirname(__dirname), '.env'),
