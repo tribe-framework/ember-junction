905a5fd9b18f0cc99d7d593da4053637f14a72f1
Apurv Jyotirmay <mail@jyotirmay.dev>
2024-01-15 02:51:37 +0530

    fix: better accessibility with numeric fields

--- blueprints/ember-junction/files/app/components/input-fields/text.js
+++ blueprints/ember-junction/files/app/components/input-fields/text.js
@@ -8,4 +8,29 @@ export default class InputFieldsTextComponent extends Component {
 		let passphrase = await Passphrase.generate(36);
 		this.args.mutObjectModuleValue(this.args.module.input_slug, passphrase);
 	}
+
+  inputType = (type) => {
+    if (type === 'number') {
+      return 'text';
+    }
+
+    return type;
+  }
+
+  inputMode = (type) => {
+    switch (type) {
+      case 'password':
+        type = 'text';
+        break;
+
+      case 'number':
+        type = 'numeric';
+        break;
+
+      default:
+        break;
+    }
+
+    return type;
+  }
 }
