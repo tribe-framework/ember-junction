05811b888f19497136f1417e41f4ec9773e0aac5
cityowl / moti <cityowl@hey.com>
2024-04-20 16:42:41 +0530

    HIDE_POSTCODE_ATTRIBUTION

--- blueprints/ember-junction/files/app/components/footer.js
+++ blueprints/ember-junction/files/app/components/footer.js
@@ -1,6 +1,14 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import ENV from '<%= dasherizedPackageName %>/config/environment';
 
 export default class FooterComponent extends Component {
   @tracked yearNow = new Date().getFullYear();
+
+  get hidePostcodeAttribution() {
+    if (ENV.HIDE_POSTCODE_ATTRIBUTION !== undefined)
+      return ENV.HIDE_POSTCODE_ATTRIBUTION;
+    else
+      return false;
+  }
 }
