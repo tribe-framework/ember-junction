ae856be2522b208f523100bc2a5868a5f5eb4f92
cityowl / moti <cityowl@hey.com>
2024-04-29 23:53:41 +0530

    shifting to fontawesome free

--- blueprints/ember-junction/files/config/dotenv.js
+++ blueprints/ember-junction/files/config/dotenv.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 module.exports = function (/* env */) {
   return {
-    clientAllowedKeys: ['JUNCTION_PASSWORD', 'PLAUSIBLE_AUTH', 'PLAUSIBLE_DOMAIN', 'PUSHER_API_KEY', 'PUSHER_CLUSTER'],
+    clientAllowedKeys: ['JUNCTION_PASSWORD', 'PLAUSIBLE_AUTH', 'PLAUSIBLE_DOMAIN', 'PUSHER_API_KEY', 'PUSHER_CLUSTER', 'HIDE_POSTCODE_ATTRIBUTION'],
     fastbootAllowedKeys: [],
     failOnMissingKey: false,
     path: path.join(path.dirname(__dirname), '.env'),
