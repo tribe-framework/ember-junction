73c7eedd4dd569ce979de936403b6e6ce1386461
cityowl / jigmet <cityowl@hey.com>
2024-07-03 21:30:48 +0530

    customHash null

---
+++ blueprints/ember-junction/files/ember-cli-build.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function (defaults) {
+  const app = new EmberApp({
+    fingerprint: {
+      customHash: null,
+    }
+  }, {
+    // Add options here
+  });
+
+  return app.toTree();
+};
