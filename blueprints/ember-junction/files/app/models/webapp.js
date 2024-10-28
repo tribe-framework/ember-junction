d0a98f8d88d512343e4f65d6676fef0fa15e34d4
cityowl / moti <cityowl@hey.com>
2024-10-28 18:07:18 +0530

    auth submit issue

--- blueprints/ember-junction/files/app/models/webapp.js
+++ blueprints/ember-junction/files/app/models/webapp.js
@@ -1,1 +1,6 @@
-import Model, { attr } from '@ember-data/model'; export default class WebappModel extends Model { @attr     slug; @attr modules; }
+import Model, { attr } from '@ember-data/model';
+
+export default class WebappModel extends Model {
+  @attr slug;
+  @attr modules;
+}
