40636ecfa59f8fcbd1f4e3743bbcfe59ced27031
cityowl / jigmet <cityowl@hey.com>
2025-06-11 18:23:15 +0530

    Junction Tribe keys config

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = function (/* environment, appConfig */) {
-  return {};
+  let ENV = {
+    //Junction Tribe config for EmberJS
+    TribeENV: {
+      API_URL: process.env.TRIBE_API_URL,
+      API_KEY: process.env.TRIBE_API_KEY,
+    },
+  };
+
+  return ENV;
 };
